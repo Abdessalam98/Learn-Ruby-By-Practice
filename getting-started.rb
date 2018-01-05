ac33c051b30468e1fd459e3f977d73dffa3cb1e5
Abdessalam98 <belmehelabdessalam.benharira@ynov.com>
2018-01-05 09:13:46 +0100

    add first ex

---
+++ getting-started.rb
@@ -0,0 +1,27 @@
+puts "Hello World" #string
+puts 10 #integer
+puts "10" + "10" #will return 1010 (concatenation)
+puts "10".to_i + 10 #will return 20
+
+puts "Hello"
+puts "again"
+print "Hello" #print doesn't add new line character
+print " again\n" 
+
+
+i,j,k =1,8,10
+
+puts i
+puts j
+puts k # same as puts i,j,k
+puts i,j,k
+
+#change values
+
+m =2
+n=4
+
+m,n=n,m
+
+puts m #will be 4
+puts n #will be 2
