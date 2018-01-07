7e84d112ef0c237c5f3f2cfd583ba6a206782388
Abdessalam98 <belmehelabdessalam.benharira@ynov.com>
2018-01-07 14:55:04 +0100

    add exo on string interpolation

---
+++ exostring_interpolation.rb
@@ -0,0 +1,19 @@
+#interpolation will embed value of variable into the string
+
+name = "Abdessalam"
+job= "Web Developer"
+
+
+puts "Hello my name is #{name} and I'm #{job}"
+
+# Note: don't use interpolation with single quotes it won't work
+
+puts 'Hello my name is #{name} and I\'m #{job}'
+
+
+# date and operations
+
+puts "Today we are : #{Time.now}"
+
+puts "5 + 6 = #{5+6}"
+
